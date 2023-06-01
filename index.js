afb50b75f45ea848892c89c1bba672f41fe2ed0d
Davis <droldan2019310@kinal.edu.gt>
2023-05-31 19:02:55 -0600

    update server index

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 'use strict'
-
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var app = express();
+var app = require('./app');
+var port = 49158;
 
 
-var shoesRoute = require("./routes/shoes.route");
 
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-	next();
-});
-
-
-app.use("/shoes",shoesRoute);
-
-module.exports = app;
+app.listen(port, ()=>{            
+    console.log('Servidor de express corriendo');                        
+})
